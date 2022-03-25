3e7abeb776ec3b5c9a02dc14d193c5591d958db5
oArthur <arthursouzamaciel@gmail.com>
2022-03-25 19:10:19 -0300

    Removendo linhas desnecessarias e adicionado comentarios

--- src/twitter-api.py
+++ src/twitter-api.py
@@ -1,4 +1,6 @@
 import tweepy
+
+#Pegando as KEYS do arquivo twitter-keys.py e lendo linha a linha. Logo depois realizamos a autenticação.
 with open('./src/twitter-tokens.txt', 'r') as tfile:
     access_token = tfile.readline().strip("\n")
     access_token_secret = tfile.readline().strip("\n")
@@ -8,5 +10,3 @@ with open('./src/twitter-tokens.txt', 'r') as tfile:
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-
-public_tweets = api.home_timeline()
