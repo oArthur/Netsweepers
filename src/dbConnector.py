ea185fd048423814cbfbec7bd3ee7bc9bb4663f0
Vinícius Carvalho <valves8956@gmail.com>
2022-03-29 16:18:43 -0300

    Inicialização CRUD

--- src/dbConnector.py
+++ src/dbConnector.py
@@ -5,22 +5,42 @@ from twitterSearch import buscarTweets
 # Função para criar database
 def createdb(dbname, cursor):
     try:
-        cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(dbname))
+        cursor.execute("CREATE DATABASE IF NOT EXISTS {};".format(dbname))
     except mysql.connector.Error as err:
-            print("Erro ao criar banco de dados: {}".format(err))
+            print("Erro ao criar banco de dados: {};".format(err))
 # Função para criar tabela
 def createTable(tbname, table_structure, cursor):
     try:
-        cursor.execute("CREATE TABLE IF NOT EXISTS {}({})".format(tbname,table_structure))
+        cursor.execute("CREATE TABLE IF NOT EXISTS {}({});".format(tbname,table_structure))
     except mysql.connector.Error as err:
-            print("Erro ao criar banco de dados: {}".format(err))
+            print("Erro ao criar tabela: {}".format(err))
 # Função para inserir dados
 def insertValues(tbname,data,cursor):
     try:
-        cursor.execute("INSERT INTO {} VALUES({})".format(tbname,data))
+        cursor.execute("INSERT INTO {} VALUES({});".format(tbname,data))
     except mysql.connector.Error as err:
-        print("Erro ao criar banco de dados: {}".format(err))
-
+        print("Erro ao inserir dados: {}".format(err))
+# Função ara deletar valores
+def deleteValues(tbname,deleting,cursor):
+    try:
+        print(cursor.execute("SELECT * FROM {} WHERE idt={};".format(tbname,deleting)))
+        confirm = int(input("\nVocê confirma a exclusão dos dados acima?\n 1 - Sim | 2 - Não"))
+        if confirm==1:
+            cursor.execute("DELETE FROM {} WHERE idt={}".format(tbname,deleting))
+        elif confirm==2:
+            pass
+        else:
+            print("Opção inválida")
+    except mysql.connector.Error as err:
+        print("Erro ao deletar dados: {}".format(err))
+    except TypeError as err:
+        print("Valor inválido digitado.")
+# Funçã para buscar dados (NÃO IMPLEMENTADO)
+def searchValues(tbname, index, cursor):
+    try:
+        pass
+    except mysql.connector.Error as err:
+        print("Erro na operação: {}".format(err))
 # Funções 'main' abaixo estão sendo utilizadas para finalidade de testes, serão removidos na versão final
 if __name__ == '__main__':
     cnx = mysql.connector.connect(user='****', password='********', host='127.0.0.1', database='twitterdb')
